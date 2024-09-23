55105e6f77c89d1770b8823a13d6f6e8f37445a8
JuanRNS <juanramalho90@gmail.com>
2024-09-22 22:56:59 -0300

    ultima atulização

--- DesafioSite/script.js
+++ DesafioSite/script.js
@@ -1,5 +1,15 @@
 let btn = document.getElementById("btn")
-
+let btntext = document.getElementById("btntext")
+let btnicon = document.getElementById("btnicon")
 btn.onclick = function(){
     document.body.classList.toggle("dark-theme")
+
+    if(document.body.classList.contains("dark-theme")){
+        btnicon.src = "sun.png"
+        btntext.innerHTML ="Light"
+    }else{
+        btnicon.src = "moon.png"
+        btntext.innerHTML = "Dark"
+
+    }
 }
