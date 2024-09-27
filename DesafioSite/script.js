00d0730c2b66049bf04c863e3135583951215cc2
JuanRNS <juanramalho90@gmail.com>
2024-09-26 23:46:14 -0300

    ultima

--- DesafioSite/script.js
+++ DesafioSite/script.js
@@ -1,13 +1,13 @@
 let btn = document.getElementById("btn")
 let btntext = document.getElementById("btntext")
 let btnicon = document.getElementById("btnicon")
-btn.onclick = function(){
+btn.onclick = function () {
     document.body.classList.toggle("dark-theme")
 
-    if(document.body.classList.contains("dark-theme")){
+    if (document.body.classList.contains("dark-theme")) {
         btnicon.src = "sun.png"
-        btntext.innerHTML ="Light"
-    }else{
+        btntext.innerHTML = "Light"
+    } else {
         btnicon.src = "moon.png"
         btntext.innerHTML = "Dark"
 
