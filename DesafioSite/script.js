e73faeac6f75c499b157a9b572bac9441ce18404
JuanRNS <juanramalho90@gmail.com>
2024-09-28 14:29:12 -0300

    ultima

--- DesafioSite/script.js
+++ DesafioSite/script.js
@@ -1,15 +1,22 @@
-let btn = document.getElementById("btn")
-let btntext = document.getElementById("btntext")
-let btnicon = document.getElementById("btnicon")
+let btn = document.getElementById("btn");
+let btntext = document.getElementById("btntext");
+let btnicon = document.getElementById("btnicon");
+let header = document.querySelector("header");
+ 
 btn.onclick = function () {
-    document.body.classList.toggle("dark-theme")
-
+    document.body.classList.toggle("dark-theme");
+ 
     if (document.body.classList.contains("dark-theme")) {
-        btnicon.src = "sun.png"
-        btntext.innerHTML = "Light"
+        header.classList.remove("cabeca");
+        header.classList.ad("dark-header");
+ 
+        btnicon.src = "sun.png";
+        btntext.innerHTML = "Light";
     } else {
-        btnicon.src = "moon.png"
-        btntext.innerHTML = "Dark"
-
+        header.classList.remove("dark-header");
+        header.classList.add("cabeca");
+ 
+        btnicon.src = "moon.png";
+        btntext.innerHTML = "Dark";
     }
-}
+};
